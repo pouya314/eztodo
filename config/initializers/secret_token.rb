d20c8e31ce0d617176113c5cfbe49579db9c6332
Pouya Arvandian <pouya314@gmail.com>
2013-06-15 14:05:19 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eztodo::Application.config.secret_token = '358483d5b2c2d8d3b4ea6bf7dc5bacc3b11d361f12348d0ce907ad0301bc58735a94903a71eda1d32c81406664219b67150d0c720a4423656db896a2e01daa10'
